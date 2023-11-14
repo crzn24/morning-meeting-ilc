dbe8218d27170a2268dd12bcf655541848921afb
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-11-13 20:46:47 -0800

    Starting over with calendar. Installs fullcalendar dependencies

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,29 +1,43 @@
-import React from "react";
-import {
-  add,
-  differenceInDays,
-  endOfMonth,
-  format,
-  setDate,
-  startOfMonth,
-  sub,
-} from "date-fns";
-import CalendarCell from "./CalendarCell";
-
-const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+import React from 'react'
+// import Fullcalendar from "@fullcalendar/react";
+// import dayGridPlugin from "@fullcalendar/daygrid";
+// import timeGridPlugin from "@fullcalendar/timegrid";
+// import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = () => {
   return (
-    <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full border-t border-l">
-      <div className="grid grid-cols-7 items-center justify-center text-center">
-        <div>{'<<'}</div>
-        <div>{'<'}</div>
-        <div className="col-span-3">November 2023</div>
-        <div>{'>'}</div>
-        <div>{'>>'}</div>
-      </div>
-    </div>
-  );
-};
+    <div>Calendar</div>
+  )
+}
 
-export default Calendar;
+export default Calendar
+
+// import React from "react";
+// import {
+//   add,
+//   differenceInDays,
+//   endOfMonth,
+//   format,
+//   setDate,
+//   startOfMonth,
+//   sub,
+// } from "date-fns";
+// import CalendarCell from "./CalendarCell";
+
+// const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// const Calendar = () => {
+//   return (
+//     <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full border-t border-l">
+//       <div className="grid grid-cols-7 items-center justify-center text-center">
+//         <div>{'<<'}</div>
+//         <div>{'<'}</div>
+//         <div className="col-span-3">November 2023</div>
+//         <div>{'>'}</div>
+//         <div>{'>>'}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Calendar;
--- src/components/CalendarCell.jsx
+++ src/components/CalendarCell.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-const CalendarCell = () => {
+// const CalendarCell = () => {
 
-  return (
-    <div>
+//   return (
+//     <div>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default CalendarCell
+// export default CalendarCell
